b23991d032d0c779acd0a9ca43f855951d060cb0
SHsamir <shammouti@odiso.com>
2016-02-29 15:46:27 +0000

    je viens de rajouter SHsamir.py

---
+++ SHsamir.py
@@ -0,0 +1,31 @@
+
+
+nombre = raw_input('1er nombre : ')
+
+nombre2 = raw_input('2eme nombre : ')
+
+  
+resultat = int(nombre) + int(nombre2)
+
+print "addition :"
+
+print resultat
+
+resultat = int(nombre) - int(nombre2)
+
+print "soustraction :"
+
+print resultat
+
+resultat = int(nombre) * int(nombre2)
+
+print "multiplication :"
+
+print resultat
+
+resultat = int(nombre) / int(nombre2)
+
+print "division :"
+
+print resultat
+
